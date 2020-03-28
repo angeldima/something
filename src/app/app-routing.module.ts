f3e2010b4bcda3b4be5e7b49467c14ee1d6d6379
Angelo Di Martino <angelo.dimartino@kalpa.it>
2020-03-28 11:37:29 +0100

    Added route module and base-layout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,27 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomepageComponent } from './pages/homepage/homepage.component';
+import { Page1Component } from './pages/page1/page1.component';
+import { Page2Component } from './pages/page2/page2.component';
+import { Page3Component } from './pages/page3/page3.component';
+import { LayoutComponent } from './ui-template/layout/layout.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      { path: '', redirectTo: '/homepage', pathMatch: 'full' },
+      { path: 'homepage', component: HomepageComponent },
+      { path: 'page1', component: Page1Component },
+      { path: 'page2', component: Page2Component },
+      { path: 'page3', component: Page3Component }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,25 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { AmplifyAngularModule, AmplifyService } from 'aws-amplify-angular';
+import { HeaderComponent } from './ui-template/header/header.component';
+import { FooterComponent } from './ui-template/footer/footer.component';
+import { LayoutComponent } from './ui-template/layout/layout.component';
+import { HomepageComponent } from './pages/homepage/homepage.component';
+import { Page1Component } from './pages/page1/page1.component';
+import { Page2Component } from './pages/page2/page2.component';
+import { Page3Component } from './pages/page3/page3.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    LayoutComponent,
+    HomepageComponent,
+    Page1Component,
+    Page2Component,
+    Page3Component
+  ],
   imports: [BrowserModule, AppRoutingModule, AmplifyAngularModule],
   providers: [AmplifyService],
   bootstrap: [AppComponent]
